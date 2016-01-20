7fc8567134728cc75eb08c07fe3750f41ca70a4e
Isman Firmansyah <isman.firmansyah@gmail.com>
2016-01-20 13:12:29 +0700

    fix install-pkgs.sh script

--- gluu-cluster-centos/volumes/consumer/install-pkgs.sh
+++ gluu-cluster-centos/volumes/consumer/install-pkgs.sh
@@ -1,6 +1,4 @@
 #!/usr/bin/env sh
-yum install -y gluu-consumer
-# systemctl start docker.service
-# systemctl enable docker.service
-# systemctl start salt-minion.service
-# systemctl enable salt-minion.service
+yum install -y gluu-consumer #gluu-agent
+systemctl start docker.service && systemctl enable docker.service
+systemctl start salt-minion.service && systemctl enable salt-minion.service
--- gluu-cluster-centos/volumes/master/install-pkgs.sh
+++ gluu-cluster-centos/volumes/master/install-pkgs.sh
@@ -1,10 +1,6 @@
 #!/usr/bin/env sh
-yum install -y gluu-master #gluu-flask gluu-agent
-systemctl start docker.service
-systemctl enable docker.service
-systemctl start salt-master.service
-systemctl enable salt-master.service
-systemctl start salt-minion.service
-systemctl enable salt-minion.service
-# systemctl start gluu-flask.service
-# systemctl enable gluu-flask.service
+yum install -y gluu-master gluu-flask #gluu-agent
+systemctl start docker.service && systemctl enable docker.service
+systemctl start salt-master.service && systemctl enable salt-master.service
+systemctl start salt-minion.service && systemctl enable salt-minion.service
+systemctl start gluu-flask.service && systemctl enable gluu-flask.service
