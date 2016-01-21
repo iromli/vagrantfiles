83c8a4872ebad9cd7094787a14de7a2fe5b5569d
Isman Firmansyah <isman.firmansyah@gmail.com>
2016-01-21 15:22:57 +0700

    fix install-pkgs scripts

--- gluu-cluster-centos/volumes/consumer/install-pkgs.sh
+++ gluu-cluster-centos/volumes/consumer/install-pkgs.sh
@@ -1,7 +1,12 @@
 #!/usr/bin/env sh
 yum install -y gluu-consumer
+
 systemctl start docker.service && systemctl enable docker.service
 systemctl start salt-minion.service && systemctl enable salt-minion.service
 
+firewall-cmd --permanent --add-port=2376/tcp
+firewall-cmd --reload
+
 yum install -y gluu-agent
+
 systemctl start gluu-agent.service && systemctl enable gluu-agent.service
--- gluu-cluster-centos/volumes/master/install-pkgs.sh
+++ gluu-cluster-centos/volumes/master/install-pkgs.sh
@@ -1,11 +1,20 @@
 #!/usr/bin/env sh
 yum install -y gluu-master
+
 systemctl start docker.service && systemctl enable docker.service
+
 systemctl start salt-master.service && systemctl enable salt-master.service
 systemctl start salt-minion.service && systemctl enable salt-minion.service
 
+firewall-cmd --permanent --add-port=4505-4506/tcp
+firewall-cmd --reload
+
 yum install -y gluu-flask
+
 systemctl start gluu-flask.service && systemctl enable gluu-flask.service
 
 yum install -y gluu-agent
+
 systemctl start gluu-agent.service && systemctl enable gluu-agent.service
+
+mv /usr/share/oxd-license-validator/oxd-license-validator-3.0.1-SNAPSHOT-jar-with-dependencies.jar /usr/share/oxd-license-validator/oxd-license-validator.jar
