57d53f6a2504a8457aaa043de21bdf026eeb3372
Isman Firmansyah <isman.firmansyah@gmail.com>
2016-01-22 21:48:50 +0700

    remove oxd-license-validator hack

--- gluu-cluster-centos/volumes/master/install-pkgs.sh
+++ gluu-cluster-centos/volumes/master/install-pkgs.sh
@@ -16,5 +16,3 @@ systemctl start gluu-flask.service && systemctl enable gluu-flask.service
 yum install -y gluu-agent
 
 systemctl start gluu-agent.service && systemctl enable gluu-agent.service
-
-mv /usr/share/oxd-license-validator/oxd-license-validator-3.0.1-SNAPSHOT-jar-with-dependencies.jar /usr/share/oxd-license-validator/oxd-license-validator.jar
