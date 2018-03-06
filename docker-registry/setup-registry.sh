3279de0602ad9a3e16fd7b635224b086f8ad6557
iromli <isman.firmansyah@gmail.com>
2018-03-06 22:37:11 +0700

    remove obsolete boxes

--- docker-registry/setup-registry.sh
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/env bash
-
-if [[ -z $(docker ps | grep registry) ]]; then
-    docker run -d -p 5000:5000 --name registry registry:2
-fi
-
-if [[ -z $(docker ps | grep docker-auth) ]]; then
-    docker run -d -p 5001:5001 --name docker-auth cesanta/docker_auth
-fi
